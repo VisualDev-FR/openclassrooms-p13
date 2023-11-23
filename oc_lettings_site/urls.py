d3f68b9627c566a9d7d2f5d103f8303db7e294de
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2023-11-23 13:54:37 +0100

    move views in dedicated apps

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+from oc_lettings_site import views as index
+from profiles import views as profiles
+from lettings import views as lettings
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('', index.index, name='index'),
+    path('lettings/', lettings.lettings_index, name='lettings_index'),
+    path('lettings/<int:letting_id>/', lettings.letting, name='letting'),
+    path('profiles/', profiles.profiles_index, name='profiles_index'),
+    path('profiles/<str:username>/', profiles.profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
