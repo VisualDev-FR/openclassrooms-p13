ca871a9e6aaf9262b8402daf583b1aa8d2973a64
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2023-12-01 16:24:21 +0100

    migrate app secret key in environement variable

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -10,12 +10,11 @@ sentry_sdk.init(
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
+SECRET_KEY = os.environ.get("OC_LETTING_SK")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
