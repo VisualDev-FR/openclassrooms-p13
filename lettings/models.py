e2f806c20b9c7ed21f4d3858d11521663a6422a4
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2023-12-01 13:58:32 +0100

    create documentation of all classes and functions

--- lettings/models.py
+++ lettings/models.py
@@ -3,6 +3,27 @@ from django.core.validators import MaxValueValidator, MinLengthValidator
 
 
 class Address(models.Model):
+    """
+    Data object storing all address details
+
+    Attributes
+    -------
+    number: int
+        the street number of the address
+
+    street: str
+        the street of the address
+
+    state: str
+        the state of the address (2 digits)
+
+    zip_code: int
+        the zip code of the address (int between 0 and 99999)
+
+    country_iso_code: str
+        iso code of the address's country (3 chars)
+    """
+
     number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
     street = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
@@ -13,6 +34,9 @@ class Address(models.Model):
     )
 
     def __str__(self):
+        """
+        Gives a string representation of an Address Object.
+        """
         return f"{self.number} {self.street}"
 
     class Meta:
@@ -20,8 +44,23 @@ class Address(models.Model):
 
 
 class Letting(models.Model):
+    """
+    Data object storing letting informations
+
+    Attributes
+    -------
+    title : str
+        the title of the letting
+
+    address : Address
+        A reference to an Address object
+    """
+
     title = models.CharField(max_length=256)
     address = models.OneToOneField(Address, on_delete=models.CASCADE)
 
     def __str__(self):
+        """
+        Gives a string representation of a Letting Object.
+        """
         return self.title
--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render
 
 
+# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis
+# leo consectetur ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis,
+# sem mi convallis eros, vitae dapibus nisi lorem dapibus sem. Maecenas pharetra purus ipsum,
+# eget consequat ipsum lobortis quis. Phasellus eleifend ex auctor venenatis tempus. Aliquam
+# vitae erat ac orci placerat luctus. Nullam elementum urna nisi, pellentesque iaculis enim
+# cursus in. Praesent volutpat porttitor magna, non finibus neque cursus id.
 def index(request):
     """
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis
-    leo consectetur ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis,
-    sem mi convallis eros, vitae dapibus nisi lorem dapibus sem. Maecenas pharetra purus ipsum,
-    eget consequat ipsum lobortis quis. Phasellus eleifend ex auctor venenatis tempus. Aliquam
-    vitae erat ac orci placerat luctus. Nullam elementum urna nisi, pellentesque iaculis enim
-    cursus in. Praesent volutpat porttitor magna, non finibus neque cursus id.
+    Display the home page of the website
     """
     return render(request, "index.html")
 
 
 def handler404(request):
+    """
+    Custom view displayed when a 404 error occurs.
+    """
     return render(request, "404.hmtl", status=404)
 
 
 def handler500(request):
+    """
+    Custom view displayed when a 500 error occurs
+    """
     return render(request, "500.hmtl", status=500)
