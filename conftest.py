c25ecf4d82d20958f1f8c3c6a877faeaaf052d0f
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2023-12-01 14:33:44 +0100

    implement single letting view tests

--- conftest.py
+++ conftest.py
@@ -1,10 +1,12 @@
+from django.conf import settings
 import pytest
 
 
 @pytest.fixture(scope="session")
 def django_db_setup():
-    from django.conf import settings
-
+    """
+    Fixture allowing to duplicate the given database for testing
+    """
     settings.DATABASES["default"] = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": "oc-lettings-site.sqlite3",
--- lettings/tests.py
+++ lettings/tests.py
@@ -15,3 +15,27 @@ def test_lettings_index():
     assert response.status_code == 200
     assert b"Joshua Tree Green Haus /w Hot Tub" in response.content
     assert b"Oceanview Retreat" in response.content
+
+
+@pytest.mark.django_db
+def test_letting_single_view():
+    """
+    Test that single letting views are correctly displayed.
+    """
+    client = Client()
+
+    # test letting 1
+    response = client.get("/lettings/1/")
+
+    assert response.status_code == 200
+    assert b"Joshua Tree Green Haus /w Hot Tub" in response.content
+    assert b"7217 Bedford Street" in response.content
+    assert b"Brunswick, GA 31525" in response.content
+
+    # test letting 3
+    response = client.get("/lettings/3/")
+
+    assert response.status_code == 200
+    assert b"&#x27;Silo Studio&#x27; Cottage" in response.content
+    assert b"340 Wintergreen Avenue" in response.content
+    assert b"Newport News, VA 23601" in response.content
