d41d259e22514feb60d88d44e1912b044bc8d70b
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2023-12-11 15:39:38 +0100

    update models docstrings

--- profiles/models.py
+++ profiles/models.py
@@ -5,17 +5,15 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     """
     Data object storing the tenants Profile informations.
-
-    Attributes
-    -------
-    user : User
-        a reference to an user
-
-    favorite_city : str
-        A string representing the favorite city of the user
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    """
+    A reference to an user
+    """
     favorite_city = models.CharField(max_length=64, blank=True)
+    """
+    A string representing the favorite city of the user
+    """
 
     def __str__(self):
         """
